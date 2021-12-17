6c384115cd4947936ab03b2498c59b3c488756b2
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2021-12-17 10:05:32 +0900

    fix capacity provider
    and add tests for ECS config.

--- config.go
+++ config.go
@@ -18,23 +18,64 @@ type Config struct {
 }
 
 type ECSCfg struct {
-	Region                   string                              `yaml:"region"`
-	Cluster                  string                              `yaml:"cluster"`
-	CapacityProviderStrategy []*ecs.CapacityProviderStrategyItem `yaml:"capacity_provider_strategy"`
-	LaunchType               *string                             `yaml:"launch_type"`
-	NetworkConfiguration     NetworkConfiguration                `yaml:"network_configuration"`
-	DefaultTaskDefinition    string                              `yaml:"default_task_definition"`
-	EnableExecuteCommand     bool                                `yaml:"enable_execute_command"`
+	Region                   string                   `yaml:"region"`
+	Cluster                  string                   `yaml:"cluster"`
+	CapacityProviderStrategy CapacityProviderStrategy `yaml:"capacity_provider_strategy"`
+	LaunchType               *string                  `yaml:"launch_type"`
+	NetworkConfiguration     *NetworkConfiguration    `yaml:"network_configuration"`
+	DefaultTaskDefinition    string                   `yaml:"default_task_definition"`
+	EnableExecuteCommand     *bool                    `yaml:"enable_execute_command"`
+}
+
+type CapacityProviderStrategy []*CapacityProviderStrategyItem
+
+func (s CapacityProviderStrategy) toSDK() []*ecs.CapacityProviderStrategyItem {
+	if len(s) == 0 {
+		return nil
+	}
+	var items []*ecs.CapacityProviderStrategyItem
+	for _, item := range s {
+		items = append(items, item.toSDK())
+	}
+	return items
+}
+
+type CapacityProviderStrategyItem struct {
+	CapacityProvider *string `yaml:"capacity_provider"`
+	Weight           *int64  `yaml:"weight"`
+	Base             *int64  `yaml:"base"`
+}
+
+func (i CapacityProviderStrategyItem) toSDK() *ecs.CapacityProviderStrategyItem {
+	return &ecs.CapacityProviderStrategyItem{
+		CapacityProvider: i.CapacityProvider,
+		Weight:           i.Weight,
+		Base:             i.Base,
+	}
 }
 
 type NetworkConfiguration struct {
 	AwsVpcConfiguration *AwsVpcConfiguration `yaml:"awsvpc_configuration"`
 }
 
+func (c *NetworkConfiguration) toSDK() *ecs.NetworkConfiguration {
+	return &ecs.NetworkConfiguration{
+		AwsvpcConfiguration: c.AwsVpcConfiguration.toSDK(),
+	}
+}
+
 type AwsVpcConfiguration struct {
 	AssignPublicIp *string   `yaml:"assign_public_ip"`
 	SecurityGroups []*string `yaml:"security_groups"`
 	Subnets        []*string `yaml:"subnets"`
+}
+
+func (c *AwsVpcConfiguration) toSDK() *ecs.AwsVpcConfiguration {
+	return &ecs.AwsVpcConfiguration{
+		AssignPublicIp: c.AssignPublicIp,
+		Subnets:        c.Subnets,
+		SecurityGroups: c.SecurityGroups,
+	}
 }
 
 type Host struct {
